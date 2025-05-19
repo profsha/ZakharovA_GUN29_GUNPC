6cb52a2970a345ac42bd48eb9b0be23db42419e0
profsha <profsha007@gmail.com>
2025-05-19 15:15:10 +0300

    binary calculator

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+var availableOperators = new [] {"&", "|", "^"};
+Console.WriteLine("Введите первое число");
+if (int.TryParse(Console.ReadLine(), out int a))
+{
+    Console.WriteLine("Введите второе число");
+    if (int.TryParse(Console.ReadLine(), out int b))
+    {
+        Console.WriteLine("Введите оператор & | или ^");
+        var currentOperator = Console.ReadLine();
+        if (availableOperators.Any(x => currentOperator == x.Trim()))
+        {
+            int result;
+            switch (currentOperator)
+            {
+                case "&":
+                    result =  a&b;
+                    Console.WriteLine($"Результат: {result:D} {result:B} {result:X}");
+                    break;
+                case "|":
+                    result =  a|b;
+                    Console.WriteLine($"Результат: {result:D} {result:B} {result:X}");
+                    break;
+                case "^":
+                    result =  a^b;
+                    Console.WriteLine($"Результат: {result:D} {result:B} {result:X}");
+                    break;
+            }
+
+            return;
+        }
+    }
+}
+
+Console.WriteLine("Error!"); // Ваш текст
+
